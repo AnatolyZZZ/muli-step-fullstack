888a2d5e73aaa6b31d1413b0f9a4345bc843a12a
AnatolyZZZ <anatoly.zykov@gmail.com>
2023-06-26 12:40:06 +0300

    ready to deploy

--- controllers/Answers.js
+++ controllers/Answers.js
@@ -2,15 +2,16 @@ import { insertAnswer } from "../modules/Answers.js";
 
 export const _insertAnswer = async (req, res) => {
     try {
-        console.log('req.body',req.body)
+        // console.log('req.body',req.body)
         const answer = await insertAnswer ({...req.body});
-        console.log('answer', answer)
+        // console.log('answer', answer)
         if (answer.length > 0) {
             res.status(200).json({msg : "information added"})
         } else {
             res.status(500).json({msg: 'database error'})
         }
-    } catch {
+    } catch (err) {
+        // console.log('error =>',err)
         res.status(500).json({msg: 'database error'})
     }
 }
--- modules/Answers.js
+++ modules/Answers.js
@@ -1,10 +1,13 @@
 import {db} from '../config/db.js';
 
 export const insertAnswer = async (body) => {
-    console.log('body in insertAnswer', body)
-    console.log('fields in insertAnswer', body.fields)
-    const {email, name, phone} = {...body.fields}
-    return db('AllUsers')
-    .insert({email, name, phone, addons : multichoce[add_ons], ...options, yearly})
+    // console.log('body in insertAnswer', body)
+    // console.log('fields in insertAnswer', body.fields)
+    // console.log('pltypy addons =>',typeof(body.multichoice.add_ons))
+    const dataToInsert = {...body.fields, addons : JSON.stringify(body.multichoice.add_ons), yearly : body.yearly, plan : body.options.plan};
+    // console.log('to insert', dataToInsert)
+    return db('allusers')
+    .insert(dataToInsert)
     .returning('*')
+    .catch(err => console.log('error', err)) 
 }
